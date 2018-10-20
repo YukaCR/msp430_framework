a2034eb07837ea998f46be1ae532a2cfe6e55dcc
YukaCR <yukacr@qq.com>
2018-10-20 17:48:48 +0800

    make basic function stable

--- main.cpp
+++ main.cpp
@@ -14,9 +14,32 @@
 #define UCBxSTAT UCB0STAT
 #define UCBxIFG UCB0IFG
 #define UCBxTXBUF UCB0TXBUF
-#define max_fail_loop 3000
+#define UCBxCTLW0 UCB0CTLW0
+#define UCBxRXBUF UCB0RXBUF
+#define UCBxI2COA UCB0I2COA
+#define UCBxICTL UCB0ICTL
+#define max_fail_loop 1000
+
 uint16_t fail_loop=0;
 
+void i2c_master_reset();
+void i2c_master_init();
+bool i2c_start(uint8_t address,int8_t fail_count);
+bool i2c_send_data_master(uint8_t data);
+bool i2c_send_master(uint8_t address,uint8_t cmd,uint8_t data,int8_t fail_count = 5);
+void i2c_stop_master();
+
+void i2c_master_reset(){
+    UCBxCTLW0 = 0x0101;
+    UCBxBRW = 0x0000;
+    UCBxSTAT = 0X00;
+    UCBxRXBUF = 0X00;
+    UCBxTXBUF = 0X00;
+    UCBxI2COA = 0X00;
+    UCBxI2CSA = 0X00;
+    UCBxICTL = 0X0000;
+    i2c_master_init();
+}
 void i2c_master_init(){
 	P5SEL |= BIT4 + BIT5;
 	P5SEL |= BIT2 + BIT3;
@@ -29,9 +52,11 @@ void i2c_master_init(){
    UCBxCTL1 = UCSSEL_3;
    UCBxBRW = 40u;
 }
-bool i2c_start(uint8_t address,uint8_t fail_count){
+bool i2c_start(uint8_t address,int8_t fail_count){
+	if(fail_count<0){
+		return false;
+	}
     fail_loop = 0;
-    fail_count = 0;
     UCBxIFG = 0x00;
     UCBxI2CSA = address;
     UCBxCTL0 |= UCMST;
@@ -41,6 +66,7 @@ bool i2c_start(uint8_t address,uint8_t fail_count){
         fail_loop ++;
         if(fail_loop > max_fail_loop){
             if(fail_count==0){
+                i2c_master_reset();
                 return false;
             }
             return i2c_start(address,fail_count-1);
@@ -58,17 +84,21 @@ bool i2c_send_data_master(uint8_t data){
     while(!(UCBxIFG & UCTXIFG)){
         fail_loop ++;
         if(fail_loop > max_fail_loop){
+            i2c_master_reset();
             return false;
         }
     }
     return true;
 }
-bool i2c_stop_master(){
+void i2c_stop_master(){
     UCBxCTL1 |= UCTXSTP;
     UCBxCTL0 |= UCSWRST;
 }
-bool i2c_send_master(uint8_t address,uint8_t cmd,uint8_t data,uint8_t fail_count = 10){
-    if(!i2c_start(address,fail_count)){
+bool i2c_send_master(uint8_t address,uint8_t cmd,uint8_t data,int8_t fail_count){
+	if(fail_count<0){
+		return false;
+	}
+	if(!i2c_start(address,fail_count - 1)){
         return false;
     }
     if(!i2c_send_data_master(cmd)){
@@ -77,8 +107,10 @@ bool i2c_send_master(uint8_t address,uint8_t cmd,uint8_t data,uint8_t fail_count
     if(!i2c_send_data_master(data)){
         return i2c_send_master(address,cmd,data,fail_count - 1);
     };
+    i2c_stop_master();
     return true;
 }
+
 int main(){
     WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
     i2c_master_init();
