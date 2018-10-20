a65a803a52186f10bc2e3e72140b4915fa4e5024
YukaCR <yukacr@qq.com>
2018-10-20 16:47:47 +0800

    move raw code to function

--- main.cpp
+++ main.cpp
@@ -16,15 +16,11 @@
 #define UCBxTXBUF UCB0TXBUF
 #define max_fail_loop 3000
 uint16_t fail_loop=0;
-uint8_t fail_count=0;
 
 void i2c_master_init(){
 	P5SEL |= BIT4 + BIT5;
 	P5SEL |= BIT2 + BIT3;
-	//P3OUT |= BIT0 + BIT1;
     P3SEL |= BIT0 + BIT1;
-	//P3REN |= BIT0 + BIT1;
-    
    UCS_setExternalClockSource(32768,4*Mhz);
    UCS_turnOnXT2(UCS_XT2_DRIVE_4MHZ_8MHZ);
    __delay_cycles(1000);
@@ -33,35 +29,54 @@ void i2c_master_init(){
    UCBxCTL1 = UCSSEL_3;
    UCBxBRW = 40u;
 }
-bool i2c_send_master(uint8_t address,uint8_t cmd,uint8_t data,uint8_t fail_count = 10){
+bool i2c_start(uint8_t address,uint8_t fail_count){
     fail_loop = 0;
     fail_count = 0;
     UCBxIFG = 0x00;
     UCBxI2CSA = address;
     UCBxCTL0 |= UCMST;
-    UCBxCTL1 |= UCTR + UCTXSTT ;
+    UCBxCTL0 &=~ UCSWRST;
+    UCBxCTL1 |= UCTR + UCTXSTT;
     while(!(UCBxIFG & UCTXIFG)){
         fail_loop ++;
         if(fail_loop > max_fail_loop){
             if(fail_count==0){
                 return false;
             }
-            return i2c_send_master(address,cmd,data,fail_count-1);
+            return i2c_start(address,fail_count-1);
         }
     }
     if(UCBxIFG&UCNACKIFG || UCBxIFG&UCALIFG){
-        return i2c_send_master(address,cmd,data,fail_count-1);
+        return i2c_start(address,fail_count-1);
     }
+    return true;
+}
+bool i2c_send_data_master(uint8_t data){
     fail_loop = 0;
-    UCBxTXBUF = cmd;
+    UCBxIFG = 0x00;
+    UCBxTXBUF = data;
     while(!(UCBxIFG & UCTXIFG)){
         fail_loop ++;
         if(fail_loop > max_fail_loop){
-            return i2c_send_master(address,cmd,data,fail_count-1);
+            return false;
         }
     }
-    UCBxIFG = 0x00;
+    return true;
+}
+bool i2c_stop_master(){
     UCBxCTL1 |= UCTXSTP;
+    UCBxCTL0 |= UCSWRST;
+}
+bool i2c_send_master(uint8_t address,uint8_t cmd,uint8_t data,uint8_t fail_count = 10){
+    if(!i2c_start(address,fail_count)){
+        return false;
+    }
+    if(!i2c_send_data_master(cmd)){
+        return i2c_send_master(address,cmd,data,fail_count - 1);
+    }
+    if(!i2c_send_data_master(data)){
+        return i2c_send_master(address,cmd,data,fail_count - 1);
+    };
     return true;
 }
 int main(){
